840ddaaf5b7be660166db451da00b46c4f33ba86
afpine <afpine527@gmail.com>
2026-03-04 18:34:40 +0900

    feat: 주문 완료 시 wish 삭제 기능 구현

--- src/main/java/gift/order/OrderService.java
+++ src/main/java/gift/order/OrderService.java
@@ -3,6 +3,7 @@ package gift.order;
 import gift.member.Member;
 import gift.member.MemberRepository;
 import gift.option.OptionRepository;
+import gift.wish.WishRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,17 +14,20 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final OptionRepository optionRepository;
     private final MemberRepository memberRepository;
+    private final WishRepository wishRepository;
     private final KakaoMessageClient kakaoMessageClient;
 
     public OrderService(
         OrderRepository orderRepository,
         OptionRepository optionRepository,
         MemberRepository memberRepository,
+        WishRepository wishRepository,
         KakaoMessageClient kakaoMessageClient
     ) {
         this.orderRepository = orderRepository;
         this.optionRepository = optionRepository;
         this.memberRepository = memberRepository;
+        this.wishRepository = wishRepository;
         this.kakaoMessageClient = kakaoMessageClient;
     }
 
@@ -54,7 +58,12 @@ public class OrderService {
         memberRepository.save(member);
 
         // save order
-        return orderRepository.save(new Order(option, member.getId(), request.quantity(), request.message()));
+        Order saved = orderRepository.save(new Order(option, member.getId(), request.quantity(), request.message()));
+
+        // cleanup wish
+        wishRepository.deleteByMemberIdAndProductId(member.getId(), option.getProduct().getId());
+
+        return saved;
     }
 
     public void sendKakaoMessageIfPossible(Member member, Order order) {
